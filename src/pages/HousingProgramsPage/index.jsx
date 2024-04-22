6f6fcf9ec6bdd9d3d361ea4899b49d0bffb004df
Arslan Ondar <Arslanondar2003@gmail.com>
2024-04-23 00:41:13 +0700

    mobile-commits

--- src/pages/HousingProgramsPage/index.jsx
+++ src/pages/HousingProgramsPage/index.jsx
@@ -5,22 +5,24 @@ import AddInformationBlock from "../../components/AddInformation/AddInformationB
 import DescriptionProgramsBlock from "../../components/DescriptionPrograms/DescriptionProgramsBlock";
 import ActionBlock from "../../components/PerfomAction/ActionBlock";
 
-
 const HousingProgramsPage = () => {
-    return (
-        <div className="Container_CatalogProgramsPage" >
-            <div className="Block_CP_Page">
-                <h2 className="Title_CP_Page">Каталог программ с государственной поддержкой</h2>
-                <h4 className="Second_Title_CP_Page">Краткое описание всех жилищных программ мэрии города Кызыл</h4>
-            </div>
-           <CatalogProgComponents />
-           <Mortgage />
-           <DescriptionProgramsBlock />
-           <ActionBlock />
-           <AddInformationBlock />
-        </div>
-
-    )
-}
+  return (
+    <div className="Container_CatalogProgramsPage">
+      <div className="Block_CP_Page">
+        <h2 className="Title_CP_Page">
+          Каталог программ с государственной поддержкой
+        </h2>
+        <h4 className="Second_Title_CP_Page">
+          Краткое описание всех жилищных программ мэрии города Кызыл
+        </h4>
+      </div>
+      <CatalogProgComponents />
+      <Mortgage />
+      <DescriptionProgramsBlock />
+      <ActionBlock />
+      <AddInformationBlock />
+    </div>
+  );
+};
 
 export default HousingProgramsPage;
